d5ffa30fb2a54c66b4028f16719704e94459af4d
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2024-03-11 12:57:32 -0300

    instalar native base

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { Text, View } from 'react-native';
-
 import {
   Roboto_400Regular,
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto';
 import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider } from 'native-base';
+import { Text, View } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,16 +18,18 @@ export default function App() {
   }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#202024',
-      }}
-    >
-      <StatusBar style="light" backgroundColor="transparent" translucent />
-      <Text style={{ color: '#fff' }}>Hello World!</Text>
-    </View>
+    <NativeBaseProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#202024',
+        }}
+      >
+        <StatusBar style="light" backgroundColor="transparent" translucent />
+        <Text style={{ color: '#fff' }}>Hello World!</Text>
+      </View>
+    </NativeBaseProvider>
   );
 }
