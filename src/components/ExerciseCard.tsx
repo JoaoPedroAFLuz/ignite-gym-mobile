48d1471832b5f88cf303d8cb36c5ae67e65bc3fa
João Pedro Fortunato <joaofortunato@Joaos-MacBook-Pro.local>
2024-03-14 18:04:01 -0300

    criar componente Exercise Card

---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,51 @@
+import { Entypo } from '@expo/vector-icons';
+import { HStack, Heading, Icon, Image, Text, VStack } from 'native-base';
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+
+interface ExerciseCardProps extends TouchableOpacityProps {
+  title: string;
+  description: string;
+}
+
+export function ExerciseCard({
+  title,
+  description,
+  ...rest
+}: ExerciseCardProps) {
+  return (
+    <TouchableOpacity {...rest}>
+      <HStack
+        bg="gray.500"
+        mb={3}
+        p={2}
+        pr={4}
+        rounded="md"
+        alignItems="center"
+      >
+        <Image
+          source={{
+            uri: 'https://static.tuasaude.com/media/article/ll/ae/puxada-frontal_63648_l.jpg',
+          }}
+          alt="Foto do exercício"
+          w={16}
+          h={16}
+          mr={4}
+          rounded="md"
+          resizeMode="center"
+        />
+
+        <VStack flex={1}>
+          <Heading fontSize="lg" color="white">
+            {title}
+          </Heading>
+
+          <Text mt={1} fontSize="sm" color="gray.200" numberOfLines={2}>
+            {description}
+          </Text>
+        </VStack>
+
+        <Icon as={Entypo} name="chevron-thin-right" color="gray.300" />
+      </HStack>
+    </TouchableOpacity>
+  );
+}
