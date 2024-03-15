7d55b401387af505f6219c77d3532416a2b1661a
João Pedro Fortunato <joaofortunato@Joaos-MacBook-Pro.local>
2024-03-14 23:52:17 -0300

    listar exercicios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,19 @@
-import { FlatList, VStack } from 'native-base';
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base';
 import { useState } from 'react';
 
+import { ExerciseCard } from '@components/ExerciseCard';
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 
 export function Home() {
   const [groupSelected, setGroupSelected] = useState('costas');
   const groups = ['costas', 'peito', 'bíceps', 'tríceps', 'ombro', 'pernas'];
+  const [exercises, setExercises] = useState([
+    'Puxada frontal',
+    'Remada curvada',
+    'Puxada de ombro',
+    'Remada unilateral',
+  ]);
 
   return (
     <VStack flex={1}>
@@ -23,11 +30,31 @@ export function Home() {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={groupSelected === item}
+            isActive={groupSelected.toLowerCase() === item.toLowerCase()}
             onPress={() => setGroupSelected(item)}
           />
         )}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            {groups.length === 1 ? 'Exercício' : 'Exercícios'}
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <ExerciseCard title={item} description="3 séries x 10 repetições" />
+          )}
+        />
+      </VStack>
     </VStack>
   );
 }
