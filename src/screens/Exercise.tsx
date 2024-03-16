cf7b984910540226db8897ebfdff6b2bb75a27d9
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2024-03-16 12:29:25 -0300

    criar layout da tela de exercicio

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,91 @@
-import { Center, Text } from 'native-base';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { Box, HStack, Heading, Icon, Image, Text, VStack } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+
+import { Button } from '@components/Button';
+
+import BodySVG from '@assets/body.svg';
+import RepetitionSVG from '@assets/repetitions.svg';
+import SeriesSVG from '@assets/series.svg';
 
 export function Exercise() {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
-    <Center flex={1}>
-      <Text color="gray.100">Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack pt={4} pb={8} px={8} bg="gray.600" safeAreaTop>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={Feather} name="arrow-left" size={6} color="green.500" />
+        </TouchableOpacity>
+
+        <HStack mt={4} justifyContent="space-between">
+          <Heading color="gray.100" fontSize="lg" flexShrink={1}>
+            Puxada Frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySVG />
+
+            <Text ml={1} color="gray.200" textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+
+      <VStack p={8}>
+        <Image
+          source={{
+            uri: 'https://static.tuasaude.com/media/article/ll/ae/puxada-frontal_63648_l.jpg',
+          }}
+          alt="Foto do exercício"
+          w="full"
+          h={80}
+          mb={3}
+          rounded="lg"
+          resizeMode="cover"
+        />
+
+        <Box pb={4} px={8} bg="gray.600" rounded="md">
+          <HStack
+            alignItems="center"
+            justifyContent="space-around"
+            mt={5}
+            mb={6}
+          >
+            <HStack alignItems="center">
+              <SeriesSVG />
+
+              <Text ml={2} color="gray.200" fontSize="sm">
+                3
+              </Text>
+
+              <Heading ml={2} color="gray.200" fontSize="md">
+                Séries
+              </Heading>
+            </HStack>
+
+            <HStack alignItems="center">
+              <RepetitionSVG />
+
+              <Text ml={2} color="gray.200" fontSize="sm">
+                10
+              </Text>
+
+              <Heading ml={2} color="gray.200" fontSize="md">
+                Repetições
+              </Heading>
+            </HStack>
+          </HStack>
+
+          <Button title="Marcar como realizado" />
+        </Box>
+      </VStack>
+    </VStack>
   );
 }
