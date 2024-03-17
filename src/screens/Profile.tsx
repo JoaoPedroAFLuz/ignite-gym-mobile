79f52776820e7fc5438c2f435723fe82fe95d9d1
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2024-03-17 16:46:53 -0300

    selecionar imagem de perfil

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,3 +1,5 @@
+import * as FileSystem from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker';
 import {
   Center,
   Heading,
@@ -5,6 +7,7 @@ import {
   Skeleton,
   Text,
   VStack,
+  useToast,
 } from 'native-base';
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
@@ -15,9 +18,57 @@ import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 
 const PHOTO_SIZE = 32;
+const ONE_MEGA_BYTE = 1024 * 1024;
 
 export function Profile() {
   const [isLoading, setIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    'https://github.com/JoaoPedroAFLuz.png'
+  );
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    try {
+      setIsLoading(true);
+
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri,
+          { size: true }
+        );
+
+        if (
+          photoInfo.exists &&
+          photoInfo.size &&
+          photoInfo.size > ONE_MEGA_BYTE * 5
+        ) {
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma imagem de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500',
+          });
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -37,9 +88,11 @@ export function Profile() {
             isLoaded={!isLoading}
           />
 
-          {!isLoading && <UserPhoto size={PHOTO_SIZE} />}
+          {!isLoading && (
+            <UserPhoto source={{ uri: userPhoto }} size={PHOTO_SIZE} />
+          )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text mt={2} mb={8} color="green.500" fontWeight="bold">
               Alterar foto
             </Text>
