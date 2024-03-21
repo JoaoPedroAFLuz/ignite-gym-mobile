e145eecc1516c5641bd1b50712a69b73f60129c6
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2024-03-21 20:07:43 -0300

    buscar auth token salvo no local storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,12 +3,16 @@ import { createContext, ReactNode, useEffect, useState } from 'react';
 import { SignInDTO } from '@dtos/SignInDTO';
 import { UserDTO } from '@dtos/UserDTO';
 import { api } from '@services/api';
-import { storageAuthTokenSave } from '@storage/storageAuthToken';
+import {
+  storageAuthTokenGet,
+  storageAuthTokenSave,
+} from '@storage/storageAuthToken';
 import {
   storageUserGet,
   storageUserRemove,
   storageUserSave,
 } from '@storage/storageUser';
+import { AppError } from '@utils/AppError';
 
 export interface AuthContextDataProps {
   user: UserDTO;
@@ -30,13 +34,9 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     useState(true);
   const [user, setUser] = useState({} as UserDTO);
 
-  async function storageUserAndAuthToken(user: UserDTO, token: string) {
+  async function saveUserAndAuthToken(user: UserDTO, token: string) {
     try {
       setIsLoadingUserStorageData(true);
-
-      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
-      setUser(user);
 
       await storageUserSave(user);
       await storageAuthTokenSave(token);
@@ -47,6 +47,12 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     }
   }
 
+  function updateUserAndAuthToken(user: UserDTO, token: string) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+    setUser(user);
+  }
+
   async function singIn(data: SignInDTO) {
     try {
       const response = await api.post('/sessions', data);
@@ -54,12 +60,18 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       const { user, token } = response.data;
 
       if (!user.id || !token) {
-        throw new Error();
+        throw new AppError(
+          'Erro interno no servidor. Tente novamente mais tarde.'
+        );
       }
 
-      storageUserAndAuthToken(user, token);
+      await saveUserAndAuthToken(user, token);
+
+      updateUserAndAuthToken(user, token);
     } catch (error) {
       throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
     }
   }
 
@@ -80,12 +92,13 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   async function loadUserData() {
     try {
       const user = await storageUserGet();
+      const token = await storageAuthTokenGet();
 
-      if (!user.id) {
+      if (!user.id || !token) {
         return;
       }
 
-      setUser(user);
+      updateUserAndAuthToken(user, token);
     } catch (error) {
       throw error;
     } finally {
